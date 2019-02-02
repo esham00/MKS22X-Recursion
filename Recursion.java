760ec58e0796bc17b2e14a19fcffa4ad8b3820eb
Elizabeth Sham <esham00@stuy.edu>
2019-02-02 00:59:46 -0500

    wrote calculating sqrt

--- Recursion.java
+++ Recursion.java
@@ -1,5 +1,16 @@
 public class Recursion {
-    public static double guess(double guess, double tolerance) {
-	if (
+    public static double guess(double n, double guess, double tolerance) {
+	if (n / (guess * guess) <= tolerance) {
+	    return guess;
+	}else {
+	    return guess(n, (n/guess + guess)/2, tolerance);
+	}
+    }
     public static double sqrt(double n, double tolerance) {
-	if (n 
+	if (n == 0) {
+	    return 0;
+	}else {
+	    guess(n, n/4, tolerance);
+	}
+    }
+}

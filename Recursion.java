62a38047a087fc850fda2ac5ab1fb1192891ae86
Elizabeth Sham <esham00@stuy.edu>
2019-02-02 01:02:34 -0500

    added comments

--- Recursion.java
+++ Recursion.java
@@ -1,16 +1,23 @@
 public class Recursion {
     public static double guess(double n, double guess, double tolerance) {
+	//if the guess^2 is close enough to n (determined by the tolerance), it returns the guess 
 	if (n / (guess * guess) <= tolerance) {
 	    return guess;
-	}else {
+	}
+	//conduct newton's sqrt approx
+	else {
 	    return guess(n, (n/guess + guess)/2, tolerance);
 	}
     }
     public static double sqrt(double n, double tolerance) {
+	//can't divide by zero
 	if (n == 0) {
 	    return 0;
-	}else {
+	}
+	//if n =/= 0, use the helped method, guess would be 1/4 of n
+	else {
 	    guess(n, n/4, tolerance);
 	}
     }
+    
 }

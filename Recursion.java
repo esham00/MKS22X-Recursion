18c816f92dc178ff20753c4b839b0544c899f62c
Elizabeth Sham <esham00@stuy.edu>
2019-02-02 23:57:30 -0500

    finished fib

--- Recursion.java
+++ Recursion.java
@@ -19,8 +19,22 @@ public class Recursion {
 	    return guess(n, n/4, tolerance);
 	}
     }
+    public static int linear(int term, int prev, int prev2) {
+	if (term == 0) {
+	    return prev2;
+	} else {
+	    return linear(term-1, prev+prev2, prev);
+	}
+    }
+    public static int fib(int n) {
+	return linear(n, 1, 0);
+    }
     public static void main(String[] args) {
 	System.out.println(sqrt(100.0, .000001));
 	System.out.println(sqrt(2.0, .000001));
+	for(int i = 0; i < 1000; i++) {
+	    System.out.println(fib(i));
+	}
     }
+    
 }
